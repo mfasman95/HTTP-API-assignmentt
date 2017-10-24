03b56fc4916ae0d3d54392e300ab06cf73c35fb8
Matthew Fasman <matthew.f4sm4n@gmail.com>
2017-10-24 02:36:26 -0400

    Fix small mistake

--- src/api/responses/contentResponses.js
+++ src/api/responses/contentResponses.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-const { notFound } = require('./jsonResponses');
+const jsonResponses = require('./jsonResponses');
 
 const respondContent = filePath => header => (req, res) => {
   fs.readFile(filePath, 'utf8', (err, file) => {
-    if (err) notFound(req, res);
+    if (err) jsonResponses.respond404(req, res);
     else {
       res.writeHead(200, header);
       res.end(file);
