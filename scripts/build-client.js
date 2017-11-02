d311e87d16a7c4ded73ad4af3840b8c815d4e3bd
Matthew Fasman <mfasman95@gmail.com>
2017-11-02 15:12:26 -0400

    fixed built script

--- scripts/build-client.js
+++ scripts/build-client.js
@@ -1,1 +1,2 @@
+require('child_process').spawnSync('npm', ['install'], { stdio: 'inherit', cwd: 'client', shell: true });
 require('child_process').spawnSync('npm run', ['build'], { stdio: 'inherit', cwd: 'client', shell: true });
